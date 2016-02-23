9bbb97963aa8c785ea8294981a3914c24fd7070c
Martín Alejandro <Martn Alejandro@VAIO-AGUA>
2016-02-23 19:50:17 -0300

    Commit modulo cliente

---
+++ src/modulos/Cliente.java
@@ -0,0 +1,40 @@
+/*
+ * Esta clase fué desarrollada por Martín Alejandro Fernández.
+ * La edición de la presente clase, sin expresa autorización
+ * no esta permitida.
+ */
+
+package modulos;
+
+/**
+ * Descripcion ...
+ *
+ * @author Martín Alejandro Fernández
+ * @version 1.0
+ * @see <a href="mailto://mafernandez21@hotmail.com">Contacto</a>
+ */
+public class Cliente extends Persona {
+    
+    //<editor-fold defaultstate="collapsed" desc="Atributos">
+
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Constructores">
+
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
+
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Override">
+
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Método Main">
+    public static void main(String[] args){
+        //TODO-Aquí va la lógica para iniciar la clase
+    }
+    //</editor-fold>
+
+}
