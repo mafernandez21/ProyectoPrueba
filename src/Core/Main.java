de490097d3ba2f768e2501008bb719dc9b9929a8
Martín Alejandro <Martn Alejandro@VAIO-AGUA>
2016-02-23 19:47:22 -0300

    Primer Commit

---
+++ src/Core/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Esta clase fué desarrollada por Martín Alejandro Fernández.
+ * La edición de la presente clase, sin expresa autorización
+ * no esta permitida.
+ */
+package Core;
+
+/**
+ *
+ * @author Martín Alejandro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
